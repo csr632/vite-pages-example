6512b9760bfedae2e8a87db6a8ad31dafa0c0eba
csr632 <632882184@qq.com>
2020-06-11 14:42:11 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import type { UserConfig } from 'vite'
+import * as vpr from 'vite-plugin-react'
+import pages from 'vite-plugin-react-pages'
+import { createPlugin } from 'vite-plugin-mdx'
+import remarkFrontmatter from 'remark-frontmatter'
+
+module.exports = {
+  jsx: 'react',
+  plugins: [
+    vpr,
+    createPlugin({
+      // TODO support frontmatter by default
+      remarkPlugins: [remarkFrontmatter],
+    }),
+    pages(),
+  ],
+  optimizeDeps: {
+    exclude: ['vite-pages-theme-basic'],
+  },
+  // minify: 'esbuild',
+  minify: false,
+} as UserConfig
